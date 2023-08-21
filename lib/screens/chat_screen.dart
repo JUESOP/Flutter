f7faec8082c475d10455701d577c347a4e32ed1d
JUESOP <sinangun@naver.com>
2023-08-21 17:58:13 +0900

    firebase database 연동

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -44,14 +45,35 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
             onPressed: (){
               _authentication.signOut();
-              Navigator.pop(context);
             },
           )
         ],
       ),
-      body: Center(
-        child: Text('Chat screen'),
-      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/yriQMlkGTJZnG2rmmw58/message')
+        .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index){
+              return Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  docs[index]['text'],
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              );
+            },
+          );
+        },
+      )
     );
   }
 }
